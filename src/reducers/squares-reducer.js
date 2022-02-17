3f2659e8d3e6857b30221ee868968c52b05af6de
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-02-17 10:22:17 -0800

    Remove notes
    Co-Authored-by: Zach Wilson <zwilson22495@gmail.com>

--- src/reducers/squares-reducer.js
+++ src/reducers/squares-reducer.js
@@ -10,20 +10,3 @@ export default (state = [null, null, null, null, null, null, null, null, null], 
       return state;
   }
 }
-
-/*
-export default (state = [0,0,0,0,0,0,0,0,0], action) => {
-  const { id, currentPlayer } = action;
-  switch(action.type) {
-    case 'EDIT_TILES':
-      let newState = [...state];
-      newState[id] = currentPlayer;
-      return newState;
-    case 'RESET-TILES':
-      let resetState = [0,0,0,0,0,0,0,0,0];
-      return resetState;
-    default:
-    return state;
-  }
-}
-*/
